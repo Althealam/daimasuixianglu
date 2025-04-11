4820a46b15ba52c2997073ad4912fa08e5e6c89b
althealam <althealam0531@gmail.com>
2025-04-11 20:10:37 +0800

    commit

---
+++ 动态规划/子序列问题/编辑距离/判断子序列/dp20250411.py
@@ -0,0 +1,21 @@
+# 1. dp数组以及下标的含义：dp[i][j]表示以s[i-1]为结尾的字符串，和以t[j-1]为结尾的字符串，相同子序列的长度为dp[i][j]
+# 本题可以和“最长公共子序列”联系在一起
+# 如果s是t的子序列，那么dp[len(s)-1][len(t)-1]==len(s)
+# 2. 递推公式：
+# if s[i-1]==t[j-1]: dp[i][j]=dp[i-1][j-1]+1
+# else: dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+# 3. 初始化：dp[i][0]=0 dp[0][j]=0
+# 4. 遍历顺序：先遍历s，再遍历t
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        dp=[[0]*(len(t)+1) for _ in range(len(s)+1)]
+        for i in range(1, len(s)+1):
+            for j in range(1, len(t)+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+        if dp[-1][-1]==len(s):
+            return True
+        return False
+        
