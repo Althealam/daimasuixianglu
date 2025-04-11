6ad964a39584c3cddbd6bfc92cf00f68ea437139
althealam <althealam0531@gmail.com>
2025-04-11 19:48:40 +0800

    不相交的线

---
+++ 动态规划/子序列问题/子序列（不连续）/不相交的线/dp20250411.py
@@ -0,0 +1,19 @@
+# 本题相当于找最长公共子序列
+# 1. dp数组以及下标的含义：dp[i][j]表示以[0, i-1]的nums1和[0, j-1]的nums2的最长公共子序列的长度是dp[i][j]
+# 2. 递推公式
+# if nums1[i]==nums2[j]: dp[i][j]=dp[i-1][j-1]+1
+# else: dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+# 3. 初始化：全部初始化为0
+# 4. 遍历顺序：先遍历nums1，在遍历nums2
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        dp=[[0]*(len(nums2)+1) for _ in range(len(nums1)+1)]
+        result=0 # 记录最长公共子序列的长度
+        for i in range(1, len(nums1)+1):
+            for j in range(1, len(nums2)+1):
+                if nums1[i-1]==nums2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+                result=max(dp[i][j], result)
+        return dp[-1][-1]
