cc62bd3c5414b1af1449c244dc812e151c0ffa64
althealam <althealam0531@gmail.com>
2025-04-28 13:14:02 +0800

    commit

---
+++ 回溯算法/子集类型/子集问题/回溯20250428.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        path=[]
+        self.traversal(result, path, nums, 0)
+        return result
+
+    def traversal(self, result, path, nums, startIndex):
+        result.append(path[:])
+        for i in range(startIndex, len(nums)):
+            path.append(nums[i])
+            self.traversal(result, path, nums, i+1)
+            path.pop()
