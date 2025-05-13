221574666d0f87861500f6fb69095d1e413523b6
althealam <althealam0531@gmail.com>
2025-05-13 12:48:01 +0800

    commit

---
+++ 链表/环形链表II/双指针20250513.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# 思路：
+# 1. 定义slow和fast，让fast每次走两步，slow每次走一步，最后slow和fast会在环内相遇，因此找到了相遇点
+# 2. 让head从头节点触发，当slow从相遇点出发，最后slow和head会在环的入口相遇
+# 假设slow走的节点数为x+y，fast走过的节点数为x+y+n(y+z)，n为fast指针在环内走的圈数
+# (x+y)*2=x+y+n(y+z)==>x+y=n(y+z) 
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow = head, head
+        # 寻找相遇点
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+            if slow==fast: # 找到了相遇点
+                slow=head # 让slow指向头节点
+                while slow!=fast: 
+                    slow=slow.next
+                    fast=fast.next
+                return slow
+        return None
+        
