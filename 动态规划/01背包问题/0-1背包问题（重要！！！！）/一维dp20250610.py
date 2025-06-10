e053d58c9152d164ed2341d75ce27acfe5eff285
althealam <althealam0531@gmail.com>
2025-06-10 13:06:36 +0800

    commit

---
+++ 动态规划/01背包问题/0-1背包问题（重要！！！！）/一维dp20250610.py
@@ -0,0 +1,15 @@
+# 1. dp数组以及下标的含义：dp[j]表示背包容量为j的时候可以放入的最大价值为dp[j]
+# 2. 递推公式
+# dp[j]=max(dp[j], dp[j-weight[i]]+value[i])
+# 3. 初始化：dp[0]=0
+# 4. 遍历顺序：先物品后背包，背包逆序
+n, bagweight = map(int, input().split())
+weight=list(map(int, input().split()))
+value=list(map(int, input().split()))
+
+dp=[0]*(bagweight+1)
+for i in range(n): # 遍历物品
+    for j in range(bagweight, -1, -1): # 遍历背包
+        if j>=weight[i]:
+            dp[j]=max(dp[j], dp[j-weight[i]]+value[i])
+print(dp[bagweight])
