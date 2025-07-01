0243295d39dabad541f03330c4573e24e5070617
althealam <althealam0531@gmail.com>
2025-07-01 12:47:15 +0800

    commit

---
+++ 回溯算法/排列类型/全排列 II/回溯+used20250701.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        path = []
+        nums.sort()
+        used = [False]*len(nums)
+        self.traversal(result, path, nums, used)
+        return result
+    
+    def traversal(self, result ,path, nums, used):
+        if len(path)==len(nums) and path not in result:
+            result.append(path[:])
+            return 
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+            if i>0 and nums[i]==nums[i-1] and used[i]:
+                continue
+            path.append(nums[i])
+            used[i]=True
+            self.traversal(result, path, nums, used)
+            path.pop()
+            used[i]=False
+
+        
