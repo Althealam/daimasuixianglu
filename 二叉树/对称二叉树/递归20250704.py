fac00b4221d1279d929066ebc708d3cf2d59c772
althealam <althealam0531@gmail.com>
2025-07-04 12:56:09 +0800

    commit

---
+++ 二叉树/对称二叉树/递归20250704.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# 递归法
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.compare(root.left, root.right)
+    
+    def compare(self, left, right):
+        if left is None or right is None:
+            return left is right
+        return left.val==right.val and self.compare(left.left, right.right) and self.compare(left.right, right.left)
