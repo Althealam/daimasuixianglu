df367d008d411a88aa0f39a5e45d1694a4990d43
althealam <althealam0531@gmail.com>
2025-07-04 12:54:10 +0800

    commit

---
+++ 二叉树/翻转二叉树/迭代法/迭代法20250704.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 后序遍历，先翻转左右子节点，然后再翻转根节点
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+            node.left, node.right = node.right, node.left
+        return root
