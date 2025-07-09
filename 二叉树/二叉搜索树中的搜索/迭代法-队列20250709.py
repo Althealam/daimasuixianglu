8fb47914547c3535ef790d4a1ad0bd7b63f778e8
althealam <althealam0531@gmail.com>
2025-07-09 12:44:19 +0800

    commit

---
+++ 二叉树/二叉搜索树中的搜索/迭代法-队列20250709.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        queue = collections.deque([root])
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            if node.val==val:
+                return node
+        return None
