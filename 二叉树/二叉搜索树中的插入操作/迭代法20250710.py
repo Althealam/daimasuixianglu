2598fb5f5c2f5af12f7fd110a72eb7ff5b9186e1
althealam <althealam0531@gmail.com>
2025-07-10 12:51:39 +0800

    commit

---
+++ 二叉树/二叉搜索树中的插入操作/迭代法20250710.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            node = TreeNode(val)
+            return node
+        
+        cur=root
+        parent=None
+        while cur:
+            parent = cur
+            if cur.val>val:
+                cur = cur.left
+            elif cur.val<val:
+                cur = cur.right
+            
+        node = TreeNode(val)
+        if parent.val>val:
+            parent.left = node
+        elif parent.val<val:
+            parent.right = node
+        return root
+            
