845bd0f8938bb57dcdc64a5b7cb815ddfa3b70a9
althealam <althealam0531@gmail.com>
2025-07-16 13:09:31 +0800

    commit

---
+++ 数组/在排序数组中查找元素的第一个和最后一个位置/二分20250716.py
@@ -0,0 +1,27 @@
+# 思路：通过二分查找找到第一个等于target的元素下标
+# 1. 没找到：直接返回[-1,-1]
+# 2. 找到了：左右移动指针，找到满足题目含义的区间
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans = self.erfenchazhao(nums, target)
+        if ans==-1:
+            return [-1,-1]
+        else:
+            left, right = ans, ans
+            while left-1>=0 and nums[left-1]==target:
+                left-=1
+            while right+1<=len(nums)-1 and nums[right+1]==target:
+                right+=1
+            return [left, right]
+    
+    def erfenchazhao(self, nums, target):
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid]>target:
+                right=mid-1
+            elif nums[mid]<target:
+                left=mid+1
+            else:
+                return mid
+        return -1
