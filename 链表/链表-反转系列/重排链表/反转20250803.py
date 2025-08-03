736111b97d53c3583a6b53d0fe3e810499b982ee
Althea Lam <althealam0531@gmail.com>
2025-08-03 16:39:13 +0800

    重排链表

---
+++ 链表/链表-反转系列/重排链表/反转20250803.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# 思路：
+# 1. 找到链表的中间节点
+# 2. 反转第二段链表
+# 3. 将两个链表拼接在一起
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        middle_node = self.find_mid_node(head)
+        head2 = self.reverse_list(middle_node)
+        while head2.next:
+            nxt = head.next
+            nxt2 = head2.next
+            head.next = head2
+            head2.next = nxt
+            head = nxt
+            head2 = nxt2
+
+    
+    def find_mid_node(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def reverse_list(self, head):
+        cur = head
+        pre = None
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
