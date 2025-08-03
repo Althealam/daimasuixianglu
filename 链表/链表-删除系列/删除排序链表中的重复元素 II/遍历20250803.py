a1c04616f822752e71afe48756ac0ec0e3d8bfad
Althea Lam <althealam0531@gmail.com>
2025-08-03 16:56:10 +0800

    commit

---
+++ 链表/链表-删除系列/删除排序链表中的重复元素 II/遍历20250803.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_node = ListNode(next=head)
+        cur = dummy_node
+        while cur.next and cur.next.next:
+            val = cur.next.val
+            if cur.next.val == cur.next.next.val:
+                # 开始删除节点
+                while cur.next and cur.next.val==val:
+                    cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return dummy_node.next
