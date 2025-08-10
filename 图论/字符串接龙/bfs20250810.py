6b31e723df3b66ea1f6b5992e82f76b2d65fcdcb
Althea Lam <althealam0531@gmail.com>
2025-08-10 11:19:27 +0800

    commit

--- 图论/字符串接龙/bfs20250810.py
+++ 图论/字符串接龙/bfs20250810.py
@@ -1,34 +1,37 @@
+from collections import deque
 n = int(input())
 beginStr, endStr = map(str, input().split())
 str_list = []
 for _ in range(n):
     str_list.append(str(input()))
 
-if beginStr == endStr:
+if beginStr==endStr:
     print(0)
     exit()
 
-def judge(s1, s2):
-    """判断s1到s2之间是否有一条路径"""
+def judge(str1, str2):
     count = 0
-    for i in range(len(s1)):
-        if s1[i]!=s2[i]:
+    for i in range(len(str1)):
+        if str1[i]!=str2[i]:
             count+=1
-    return count==1 # 如果只是有一个字符不一样，那么有一条路径
+    return count==1
 
-from collections import deque
-step = 1
-visited = [False for i in range(n)]
+visited = [False]*n
 queue = deque()
+step = 1
 queue.append([beginStr, step])
+found = False
 while queue:
-    str_new, step = queue.popleft()
-    if judge(str_new, endStr):
+    current_str, step = queue.popleft()
+    if judge(current_str, endStr):
+        found = True
         print(step+1)
         exit()
-    for i in range(n):
-        if not visited[i] and judge(str_list[i], str_new):
+    for i in range(len(str_list)):
+        if not visited[i] and judge(str_list[i], current_str):
             visited[i] = True
             queue.append([str_list[i], step+1])
 
-print(step)
+if not found:
+    print(0)
+    exit()
