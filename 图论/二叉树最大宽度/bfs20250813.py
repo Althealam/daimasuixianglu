f5e673b0818d10309de6092258ad9ba36a3ed72c
Althea Lam <althealam0531@gmail.com>
2025-08-13 21:23:55 +0800

    commit

--- 图论/二叉树最大宽度/bfs20250813.py
+++ 图论/二叉树最大宽度/bfs20250813.py
@@ -7,6 +7,7 @@
 
 # 时间复杂度：O(n)
 # 空间复杂度：O(n)
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,16 +16,18 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        res = 1 # 最大宽度
-        arr = [[root, 1]]
-        while arr:
-            tmp = []
-            for node, index in arr:
-                if node.left:
+        res = 1 # 最大宽度，初始化为根节点层的宽度
+        arr = [[root, 1]] # 存储当前层的节点以及编号，初始化为根节点（节点+编号1）
+        while arr: # 当当前层有节点时，继续遍历
+            tmp = [] # 存储下一层的节点以及编号
+            for node, index in arr: # 遍历当前层的所有节点，收集下一层节点
+                if node.left: # 左节点存在时，记录其编号
                     tmp.append([node.left, index*2])
-                if node.right:
+                if node.right: # 右节点存在时，记录其编号
                     tmp.append([node.right, index*2+1])
+            # 计算当前层的宽度：最右节点编号-最左节点编号+1
             res = max(res, arr[-1][1]-arr[0][1]+1)
+            # 将下一层节点转换为当前层，继续循环
             arr = tmp
         return res
 
