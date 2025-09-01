404c88a76c59946d79981064246a2954bee77c2d
Althea Lam <althealam0531@gmail.com>
2025-09-01 09:27:42 -0400

    commit

---
+++ 动态规划/回文/回文子串/dp20250901.py
@@ -0,0 +1,27 @@
+# 1. dp数组以及下标的含义：dp[i][j]表示下标为i到j的字符串是否为回文子串，如果是的话则为True，否则为False
+# 2. 递推公式
+# （1）s[i]!=s[j]: False
+# （2）s[i]==s[j]：
+# i==j: True
+# abs(i-j)==1: True
+# abs(i-j)>1: dp[i][j] = dp[i+1][j-1]
+# 3. 初始化：全部初始化为False dp[0][0]=True
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        dp = [[False]*len(s) for _ in range(len(s))]
+        dp[0][0] = True
+        res = 0 # 回文子串的数量
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i]!=s[j]:
+                    dp[i][j] = False
+                else:
+                    if i==j or j-i==1:
+                        res+=1
+                        dp[i][j]=True
+                    elif j-i>1 and dp[i+1][j-1]:
+                        res+=1
+                        dp[i][j] = dp[i+1][j-1]
+        return res
+        
