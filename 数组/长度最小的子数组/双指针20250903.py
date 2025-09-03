468d73394fb3cc8f8299daf13c839f596d2ad6ac
Althea Lam <althealam0531@gmail.com>
2025-09-03 09:06:10 -0400

    commit

---
+++ 数组/长度最小的子数组/双指针20250903.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left, right = 0, 0
+        sum_ = 0
+        ans = float('inf')
+        while right<len(nums):
+            sum_+=nums[right]
+            while sum_>=target:
+                ans = min(ans, right-left+1)
+                sum_-=nums[left]
+                left+=1
+            right+=1
+        return ans if ans!=float('inf') else 0        
