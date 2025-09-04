863eeb8b513e1536f950abe767ba800431ed6ae8
Althea Lam <althealam0531@gmail.com>
2025-09-04 11:54:17 -0400

    commit

---
+++ 数组/区间和/前缀和20250904.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.read()
+input_data = input.split()
+n = int(input_data[0])
+nums = []
+for i in range(1, n+1):
+    nums.append(int(input_data[i]))
+
+# 前缀和
+intervals = [0]*len(nums)
+intervals[0] = nums[0]
+for i in range(1, len(intervals)):
+    intervals[i] = intervals[i-1]+nums[i]
+
+index = 1+len(nums)
+while index<len(input_data):
+    interval_start = int(input_data[index])
+    interval_end = int(input_data[index+1])
+    if interval_start ==0:
+        print(intervals[interval_end])
+    else:
+        print(intervals[interval_end]-intervals[interval_start-1])
+    index+=2
+    
---
+++ 数组/在排序数组中查找元素的第一个和最后一个位置/二分20250904.py
@@ -0,0 +1,25 @@
+# 思路：通过二分查找找到其位置，然后定义left和right去找到其左右边界
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ind = self.erfenchazhao(nums, target)
+        if ind==-1:
+            return [-1, -1]
+        else:
+            left, right = ind, ind
+            while left-1>=0 and nums[left-1]==nums[left]:
+                left-=1
+            while right+1<=len(nums)-1 and nums[right+1]==nums[right]:
+                right+=1
+            return [left, right]
+    
+    def erfenchazhao(self, nums, target):
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid]<target:
+                left = mid+1
+            elif nums[mid]>target:
+                right = mid-1
+            else:
+                return mid
+        return -1
