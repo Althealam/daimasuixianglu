afb0faea5970e1fbfe873b27fa84aeffe4549da5
Althea Lam <althealam0531@gmail.com>
2025-09-19 10:03:35 -0400

    commit

---
+++ 图论/单词搜索/dfs20250919.py
@@ -0,0 +1,25 @@
+# 思路：以(i, j)为起点开始搜索，定义dfs(i, j, k)表示在当前board[i][j]这个格子，要匹配word[k]，返回在这个状态下最终能否匹配成功
+# （1）board[i][j]!=word[k]：匹配失败，返回False
+# （2）k=len(word)-1：匹配成功，返回True
+# （3）枚举(i, j)周围的四个相邻格子(x, y)，如果(x, y)没有出界，则递归(x, y, k+1)，如果其返回True，则dfs(x, y, k)也返回True
+# （4）如果递归周围的四个相邻格子都没有返回True，则最后也返回False，表示没有搜索到
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        def dfs(i, j, k):
+            if board[i][j]!=word[k]: # 匹配失败
+                return False
+            if k==len(word)-1: # 匹配成功
+                return True
+            board[i][j] = '' # 表示访问过
+            for dx, dy in directions:
+                next_x = dx+i
+                next_y = dy+j
+                if 0<=next_x<m and 0<=next_y<n and dfs(next_x, next_y, k+1):
+                    return True
+            board[i][j] = word[k] # 恢复现场
+            return False # 没有搜索到
+        return any(dfs(i, j, 0) for i in range(m) for j in range(n))
+        
---
+++ 图论/岛屿的最大周长/遍历20250919.py
@@ -0,0 +1,40 @@
+from collections import deque
+n, m = map(int, input().split())
+grid = []
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+def max_island_perimeter(grid):
+    if not grid or not grid[0]:
+        return 0
+    m, n = len(grid, len(grid[0]))
+    visited = [[False]*n for _ in range(m)]
+    directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+    def bfs(i, j):
+        q = deque()
+        q.append([i, j])
+        visited[i][j] = True
+        perimeter = 0
+        while q:
+            cur_i, cur_j = q.popleft()
+            per+=4
+            for di, dj in directions:
+                next_x, next_y = cur_i+di, cur_j+dj
+                if next_x<0 or next_y<0 or next_x>=len(grid) or next_y>=len(grid[0]):
+                    continue
+                if grid[next_x][next_y]==1:
+                    perimeter-=1
+                    if not visited[next_x][next_y]:
+                        visited[next_x][next_y]=True
+                        q.append([next_x, next_y])
+        return perimeter
+    
+    max_per = 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j]==1 and not visited[i][j]:
+                current_per = bfs(i, j)
+                if current_per>max_per:
+                    max_per = current_per
+    return max_per
