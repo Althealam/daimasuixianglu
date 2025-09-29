72b048ff9d0e3335130a8342e91eb7402489f750
Althea Lam <althealam0531@gmail.com>
2025-09-29 14:46:49 -0400

    commit

---
+++ 动态规划/回文/最长回文子串/dp20250929.py
@@ -0,0 +1,28 @@
+# 1. dp数组以及下标的含义：dp[i][j]表示s在[i, j]的范围内是否是回文子串，如果是True的话则表示是回文子串
+# 2. 递推公式
+# s[i]==s[j]: dp[i][j] = dp[i+1][j-1]
+# s[i]!=s[j]: dp[i][j] = False
+# 3. 初始化：全部初始化为False dp[i][i] = True
+# 4. 遍历顺序：j一定比i大，因此先遍历j再遍历i
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        dp = [[False]*len(s) for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = True
+        max_length = 0
+        start, end = 0, 0
+        for j in range(len(s)):
+            for i in range(j):
+                if s[i]==s[j]:
+                    if j-i<2:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i+1][j-1]
+
+                if dp[i][j]:
+                    current_length = j-i+1
+                    if current_length>max_length:
+                        max_length = current_length
+                        start, end = i, j
+        return s[start:end+1]
---
+++ 动态规划/回文/最长回文子序列/dp20250928.py
@@ -0,0 +1,18 @@
+# 1. dp数组以及下标的含义：dp[i][j]表示区间[i+1, j-1]内的最长回文子串的长度
+# 2. 递推公式：
+# （1）s[i]==s[j]: dp[i][j] = dp[i+1][j-1]+2
+# （2）s[i]!=s[j]: dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+# 3. 初始化：dp[0][0]=1 dp[i][i]=1
+# 4. 遍历顺序：i到序遍历 
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        dp = [[0]*len(s) for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = 1
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i+1, len(s)): 
+                if s[i]==s[j]:
+                    dp[i][j] = dp[i+1][j-1]+2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][-1]
